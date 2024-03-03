e0d945c6ad79d72964dd45b00b20ccdee3036814
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-03 21:13:18 +0100

    null test

---
+++ tests/null.test.js
@@ -0,0 +1,10 @@
+const sum = require('../sum.js');
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
