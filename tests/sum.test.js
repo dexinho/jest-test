6bcd58a2d0ccae19371ae5a728f9b04ba9867829
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-03 21:13:08 +0100

    moved null and zero tests to seperate files

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -4,24 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
-});
-
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
-});
-
 test('testing', () => {
   expect(sum(1, 1)).toBe(2)
 })
