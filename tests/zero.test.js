dc164103bf73f413286a3f9c61d0601172dfb12c
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-03 21:13:26 +0100

    zero test

---
+++ tests/zero.test.js
@@ -0,0 +1,10 @@
+const sum = require('../sum.js');
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
